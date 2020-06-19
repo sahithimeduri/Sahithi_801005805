9052a9861fd70276e121e19f277c7a335ac67b71
Sahithi <sahithi@sahithis-mbp.lan>
2020-06-18 22:25:26 -0400

    added font size feature

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+function increaseFontSize() {
+  document.getElementById("myBody").style.fontSize = "2.0em";
+}
+
+function decreaseFontSize() {
+  document.getElementById("myBody").style.fontSize = "1.0em";
+}
