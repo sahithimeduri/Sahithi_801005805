9ad9b4648ca3c9e6d1b14922f41a709d283dee34
Sahithi <sahithi@sahithis-mbp.lan>
2020-06-25 21:43:22 -0400

    Milestone 5 finished

--- README.md
+++
@@ -1,2 +0,0 @@
-Sahithi Meduri - 801005805
-This is the initial README file for the course
--- index.js
+++
@@ -1,9 +0,0 @@
-
-
-function increaseFontSize() {
-  document.getElementById("myBody").style.fontSize = "2.0em";
-}
-
-function decreaseFontSize() {
-  document.getElementById("myBody").style.fontSize = "1.0em";
-}
